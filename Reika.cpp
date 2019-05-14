477ba4bd8fdb9839b3a25535edb6ca2334387e46
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2019-05-14 12:00:30 -0500

    Change file names

---
+++ Reika.cpp
@@ -0,0 +1,130 @@
+#ifndef REIKA_CPP
+#define REIKA_CPP
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+class Reika{
+
+private:
+
+long Reika_Strength;
+long Reika_Agility;
+long Reika_Intelligence;
+long Reika_Vitality;
+long Reika_HP;
+long Reika_MP;
+long Reika_Concentration;
+long Reika_Boost;
+long Reika_Stamina;
+long Reika_Defense;
+long Reika_Perception;
+long Reika_Fire_Defense;
+long Reika_Water_Defense;
+long Reika_Ice_Defense;
+long Reika_Wind_Defense;
+long Reika_Earth_Defense;
+long Reika_Lightning_Defense;
+long Reika_Light_Defense;
+long Reika_Dark_Defense;
+long Reika_Attack_Speed;
+long Reika_Cast_Speed;
+long Reika_Movement_Speed;
+
+
+public:
+
+void SetReikaStrength(){
+
+}
+
+void SetReikaAgility(){
+    
+}
+
+void SetReikaIntelligence(){
+    
+}
+
+void SetReikaVitality(){
+    
+}
+
+void SetReikaHP(){
+    
+}
+
+void SetReikaMP(){
+    
+}
+
+void SetReikaConcentration(){
+    
+}
+
+void SetReikaBoost(){
+    
+}
+
+void SetReikaStamina(){
+    
+}
+
+void SetReikaDefense(){
+    
+}
+
+void SetReikaPerception(){
+    
+}
+
+void SetReikaFireDefense(){
+    
+}
+void SetReikaWaterDefense(){
+    
+}
+
+void SetReikaIceDefense(){
+    
+}
+
+void SetReikaWindDefense(){
+    
+}
+
+void SetReikaEarthDefense(){
+    
+}
+
+void SetReikaLightningDefense(){
+    
+}
+
+void SetReikaLightDefense(){
+    
+}
+
+void SetReikaDarkDefense(){
+    
+}
+
+void SetReikaAttackSpeed(){
+    
+}
+
+void SetReikaCastSpeed(){
+    
+}
+
+void SetReikaMovementSpeed(){
+    
+}
+
+
+};
+
+#endif
